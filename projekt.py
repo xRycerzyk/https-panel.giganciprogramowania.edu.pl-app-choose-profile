caece5b8d20c8e942e0a75e64696510875813b2a
HakerX8 <mateuszprzystas11@gmail.com>
2024-12-09 18:47:14 +0100

    Update projekt.py
    też tak myślę

--- projekt.py
+++ projekt.py
@@ -1,2 +1,27 @@
+import pygame
+import random
+import pygame as py
+
+SCREEN_HEIGHT
+SCREEN_WIDHT
+
+py.init
+
+gra_dziala = True
+
+while gra_dziala
+
+for zdarzenie in py.event.get():
+        if zdarzenie.type == py.KEYDOWN:
+            if zdarzenie.key == py.K_ESCAPE:
+                gra_dziala = False
+            elif zdarzenie.type == pygame.QUIT:
+            gra_dziala = False
+
+
+
+
+
+
 
 
