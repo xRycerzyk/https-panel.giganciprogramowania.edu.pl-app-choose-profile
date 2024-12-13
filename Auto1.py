1f20aa5a70f8787484f9b12bd4f109c02ad216c1
HakerX8 <mateuszprzystas11@gmail.com>
2024-12-13 17:16:59 +0100

    Update and rename Auto1 to Auto1.py

--- Auto1.py
+++ Auto1.py
@@ -2,10 +2,15 @@ import pygame
 import pygame as py
 from pygame import rect
 import copy
-
 from pygame.sprite import Group
 
+SCREEN_WIDHT = 832
+SCREEN_HEIGHT = 1000
+x = 
+y = 
 
+class Auto(pygame.sprite.Sprite)
+    def __init__(self, 
 
 
 
