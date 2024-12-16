1be3a0d5a944957adbf6462e997d424127623fe9
HakerX8 <mateuszprzystas11@gmail.com>
2024-12-16 18:17:25 +0100

    Update Auto1.py
    też tak sądze

--- Auto1.py
+++ Auto1.py
@@ -1,18 +1,36 @@
 import pygame
 import pygame as py
-from pygame import rect
+from py import rect
 import copy
-from pygame.sprite import Group
+from py.sprite import Group
+
 
 SCREEN_WIDHT = 832
 SCREEN_HEIGHT = 1000
-x = 
-y = 
+x2 = 
+y2 = 
 
-class Auto(pygame.sprite.Sprite)
-    def __init__(self, 
+class Auto(py.sprite.Sprite)
+    def __init__(self, x, y,):
+        self.pozycja = py.Rect(x, y, 96, 48)
+        self.auto = py.image.load('car1.png')
 
+        def klon(self):
+            self.auto_copy = copy.copy(self.auto)
+        
+        def maska(self):
+            maska_auta = 0
 
+            while True:
+                maska_auta = (random.randint(1, 128), random.randint(1, 128), random.randint(1, 128)) 
+
+            self.auto_copy.fill(maska_auta, special_flags=py.BLEND_ADD)
+
+            
+
+    
+    
+    
 
 
 
