13036ea68463e6e2dbf1856fc6a181ae4494dd33
xRycerzyk <ardaagols@gmail.com>
2025-01-01 14:11:14 +0100

    Update projekt.py
    ttetre

--- projekt.py
+++ projekt.py
@@ -17,7 +17,7 @@ ekran = py.display.set_mode([SCREEN_WIDHT, SCREEN_HEIGHT])
 zegar = py.time.Clock()
 obraz_tla = py.image.load("background.png")
 taksowka = py.image.load("TAXI.png")
-auto = 
+auto = Auto.klon()
 
 gra_dziala = True
 
