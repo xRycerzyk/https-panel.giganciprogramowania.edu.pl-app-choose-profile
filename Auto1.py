272dc4603975cfa75e605b4c714fc348ec34da25
xRycerzyk <ardaagols@gmail.com>
2025-01-01 15:07:50 +0100

    Update Auto1.py

--- Auto1.py
+++ Auto1.py
@@ -10,16 +10,16 @@ class Auto(py.sprite.Sprite):
     SCREEN_WIDHT = 832
     SCREEN_HEIGHT = 1000
 
-    def __init__(self, x, y,):
-        super().__self__()
-        self.rect = py.Rect(x, y, 96, 48)
+    def __init__(self, x2, y2,):
+        super().__init__()
+        self.rect = py.Rect(x2, y2, 96, 48)
         self.image = py.image.load("car1.png")
         self.auto_copy = self.image.copy()
 
     def klon(self):
-            self.auto_copy = copy.copy(self.auto)
+        self.auto_copy = copy.copy(self.auto)
         
     def maska(self):
-        maska_auta = (random.randrange(1, 128), random.randrange(1, 128), random.randrange(1, 128)) 
+        maska_auta = (random.randint(1, 128), random.randint(1, 128), random.randint(1, 128)) 
 
         self.auto_copy.fill(maska_auta , special_flags=py.BLEND_ADD)
