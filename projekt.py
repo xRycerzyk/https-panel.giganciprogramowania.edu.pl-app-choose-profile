754a9ff21df21383b3aea4fca54f55cc5c52ea00
HakerX8 <mateuszprzystas11@gmail.com>
2025-01-09 15:47:57 +0100

    Update projekt.py
    kotek

--- projekt.py
+++ projekt.py
@@ -1,8 +1,26 @@
 import pygame as py
 import random
 import time
-# from pygame.sprite import Group
-# from pygame import rect
+from pygame.sprite import Sprite
+from pygame import rect
+
+class Auto1(py.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = py.image.load('car1.png')
+        self.rect = self.image.get_rect(center = (180/2, 280/2))
+
+class Taksowka(py.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = py.image.load('TAXI.png')
+        self.rect = self.image.get_rect(center = (154/2, 280/2))
+
+class Auto2(py.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = py.image.load('car2.png')
+        self.rect = self.image.get_rect(center = (180/2, 280/2))
 
 
 SCREEN_WIDHT = 832
@@ -15,16 +33,23 @@ points = 0
 
 py.init()
 
+auto1_group = py.sprite.Group()
+auto2_group = py.sprite.Group()
+
 ekran = py.display.set_mode([SCREEN_WIDHT, SCREEN_HEIGHT])
 zegar = py.time.Clock()
 obraz_tla = py.image.load("background.png")
-taksowka = py.image.load("TAXI.png")
-auto1 = py.image.load("car1.png")
+taksowka = Taksowka()
+auto1 = Auto1()
 PORUSZ_AUTEM = py.USEREVENT+1
 py.time.set_timer(PORUSZ_AUTEM, 10)
-auto2 = py .image.load("car2.png")
+auto2 = Auto2
+
+auto1_group.add(auto1)
+auto2_group.add(auto2)
 
 gra_dziala = True
+
 #pozycja = x2, 0
 y2 = -200
 y3 = -200
@@ -33,8 +58,8 @@ x3 = random.randrange(120, 638 , 216)
 x2 = random.randrange(120, 638 , 216)
 
 while x2 == x3:
-     x3 = random.randrange(120, 638 , 216)
-     x2 = random.randrange(120, 638 , 216)
+    x3 = random.randrange(120, 638 , 216)
+    x2 = random.randrange(120, 638 , 216)
 
 pozycja = x2, y2
 pozycja1 = x3, y3
@@ -47,6 +72,11 @@ while gra_dziala:
     pozycja1 = x3, y3
     y2 += a
     y3 += b
+
+    kolizja_z_autem1 = py.sprite.spritecollideany(taksowka, auto1)
+    kolizja_z_autem2 = py.sprite.spritecollideany(taksowka, auto2)
+
+
 
     for zdarzenie in py.event.get():
         if zdarzenie.type == py.QUIT:
@@ -76,9 +106,13 @@ while gra_dziala:
         if points >= 50:
             gra_dziala = False
 
+        if kolizja_z_autem1 or kolizja_z_autem2:
+            gra_dziala = False
+
         elif zdarzenie.type == py.KEYDOWN:
             if zdarzenie.key == py.K_ESCAPE:
                 gra_dziala = False
+                
 
             if x > 120:
                 if zdarzenie.key == py.K_a:
