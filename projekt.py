805bd0a51553a6f3a3adc2f74d4b51bb23c799e2
xRycerzyk <ardaagols@gmail.com>
2025-01-12 19:20:30 +0100

    Update projekt.py

--- projekt.py
+++ projekt.py
@@ -36,6 +36,7 @@ gra_dziala = True
 y2 = -200
 y3 = -200
 
+
 x3 = random.randrange(120, 638 , 216)
 x2 = random.randrange(120, 638 , 216)
 
@@ -48,6 +49,9 @@ class Auto1():
         self.hitbox = py.Rect(self.x2, self.y2, 180, 280)
         self.image = py.image.load('car1.png')
     
+    def update_hitbox(self):
+        self.hitbox.topleft = (self.x2 , self.y2)
+    
 class Taksowka():
     def __init__(self):
         global x
@@ -56,6 +60,9 @@ class Taksowka():
         self.y = y
         self.hitbox = py.Rect(self.x, self.y, 155, 281)
         self.image = py.image.load('TAXI.png')
+    
+    def update_hitbox(self):
+        self.hitbox.topleft = (self.x , self.y)
 
 class Auto2():
     def __init__(self):
@@ -65,6 +72,9 @@ class Auto2():
         self.y3 = y3
         self.hitbox = py.Rect(self.x3, self.y3, 180, 280)
         self.image = py.image.load('car2.png')
+    
+    def update_hitbox(self):
+        self.hitbox.topleft = (self.x3 , self.y3)
 
 taksowka = Taksowka()
 auto1 = Auto1()
@@ -87,6 +97,11 @@ while gra_dziala:
     y2 += a
     y3 += b
 
+    auto1.update_hitbox()
+    taksowka.update_hitbox()
+    auto2.update_hitbox()
+        
+
     for zdarzenie in py.event.get():
         if zdarzenie.type == py.QUIT:
                 gra_dziala = False
@@ -101,6 +116,10 @@ while gra_dziala:
              
              while x3 == x2:
                 x2 = random.randrange(120, 638 , 216)
+            
+             auto1.update_hitbox()
+             taksowka.update_hitbox()
+             auto2.update_hitbox()
         
         if y3 >= 1000:
             x3 = random.randrange(120, 638 , 216)
@@ -111,6 +130,10 @@ while gra_dziala:
 
             while x3 == x2:
                  x3 = random.randrange(120, 638 , 216)
+            
+            auto1.update_hitbox()
+            taksowka.update_hitbox()
+            auto2.update_hitbox()
         
         if points >= 50:
             gra_dziala = False
@@ -124,16 +147,28 @@ while gra_dziala:
                 if zdarzenie.key == py.K_a:
                      x -= 108
                      x -= 108
+
+                auto1.update_hitbox()
+                taksowka.update_hitbox()
+                auto2.update_hitbox()
                 
             if x < 548:
                 if zdarzenie.key == py.K_d:
                     x += 108
                     x += 108
+                
+                auto1.update_hitbox()
+                taksowka.update_hitbox()
+                auto2.update_hitbox()
 
     ekran.blit(obraz_tla, (0 , 0))
     ekran.blit(taksowka.image, (taksowka.hitbox))
-    ekran.blit(auto1.image, auto1.rect)
-    ekran.blit(auto2.image, auto2.rect)
+    ekran.blit(auto1.image, (auto1.hitbox))
+    ekran.blit(auto2.image, (auto2.hitbox))
+
+    auto1.update_hitbox()
+    taksowka.update_hitbox()
+    auto2.update_hitbox()
 
     py.display.flip()
 
