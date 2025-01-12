30b2eb0515d5b33c1e8c56bedea3462ca2ef0a89
HakerX8 <mateuszprzystas11@gmail.com>
2025-01-12 18:32:31 +0100

    Update projekt.py
    kiciuś malutki

--- projekt.py
+++ projekt.py
@@ -4,23 +4,6 @@ import time
 from pygame.sprite import Group
 from pygame import rect
 
-class Auto1(py.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = py.image.load('car1.png')
-        self.rect = self.image.get_rect(center = (180/2, 280/2))
-
-class Taksowka(py.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = py.image.load('TAXI.png')
-        self.rect = self.image.get_rect(center = (154/2, 280/2))
-
-class Auto2(py.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = py.image.load('car2.png')
-        self.rect = self.image.get_rect(center = (180/2, 280/2))
 
 
 SCREEN_WIDHT = 832
@@ -33,8 +16,26 @@ points = 0
 
 py.init()
 
-auto1_group = py.sprite.Group()
-auto2_group = py.sprite.Group()
+class Auto1():
+    def __init__(self, x2, y2):
+        self.x2 = globals()[x2]
+        self.y2 = globals()[y2]
+        self.hitbox = py.Rect(self.x2, self.y2, 180, 280)
+        self.image = py.image.load('car1.png')
+    
+class Taksowka():
+    def __init__(self, x, y):
+        self.x = globals()[x]
+        self.y = globals()[y]
+        self.hitbox = py.Rect(self.x, self.y, 155, 281)
+        self.image = py.image.load('TAXI.png')
+
+class Auto2():
+    def __init__(self, x3, y3):
+        self.x3 = globals()[x3]
+        self.y3 = globals()[y3]
+        self.hitbox = py.Rect(self.x3, self.y3, 180, 280)
+        self.image = py.image.load('car2.png')
 
 ekran = py.display.set_mode([SCREEN_WIDHT, SCREEN_HEIGHT])
 zegar = py.time.Clock()
@@ -45,8 +46,9 @@ PORUSZ_AUTEM = py.USEREVENT+1
 py.time.set_timer(PORUSZ_AUTEM, 10)
 auto2 = Auto2()
 
-auto1_group.add(auto1)
-auto2_group.add(auto2)
+
+
+
 
 gra_dziala = True
 
@@ -68,15 +70,11 @@ a = random.randint(8, 13)
 b = random.randint(8, 13)
 
 while gra_dziala:
+
     pozycja = x2, y2
     pozycja1 = x3, y3
     y2 += a
     y3 += b
-
-    kolizja_z_autem1 = py.sprite.spritecollideany(taksowka, auto1_group)
-    kolizja_z_autem2 = py.sprite.spritecollideany(taksowka, auto2_group)
-
-
 
     for zdarzenie in py.event.get():
         if zdarzenie.type == py.QUIT:
@@ -91,7 +89,7 @@ while gra_dziala:
              #b = random.randint(8, 13)
              
              while x3 == x2:
-                 x2 = random.randrange(120, 638 , 216)
+                x2 = random.randrange(120, 638 , 216)
         
         if y3 >= 1000:
             x3 = random.randrange(120, 638 , 216)
@@ -104,9 +102,6 @@ while gra_dziala:
                  x3 = random.randrange(120, 638 , 216)
         
         if points >= 50:
-            gra_dziala = False
-
-        if kolizja_z_autem1 or kolizja_z_autem2:
             gra_dziala = False
 
         elif zdarzenie.type == py.KEYDOWN:
@@ -125,9 +120,9 @@ while gra_dziala:
                     x += 108
 
     ekran.blit(obraz_tla, (0 , 0))
-    ekran.blit(taksowka, (x , y))
-    ekran.blit(auto1 , (pozycja))
-    ekran.blit(auto2 , (pozycja1))
+    ekran.blit(taksowka.image, (taksowka.hitbox))
+    ekran.blit(auto1.image, auto1.rect)
+    ekran.blit(auto2.image, auto2.rect)
 
     py.display.flip()
 
