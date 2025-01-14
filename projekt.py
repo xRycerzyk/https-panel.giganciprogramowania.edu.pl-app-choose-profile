3e85eb36a12fffb82ff1758ae787e55b7bcf70cf
xRycerzyk <ardaagols@gmail.com>
2025-01-14 18:39:54 +0100

    Update projekt.py
    teraz działa

--- projekt.py
+++ projekt.py
@@ -4,19 +4,11 @@ import time
 from pygame.sprite import Group
 from pygame import rect
 
-
-
 SCREEN_WIDHT = 832
 SCREEN_HEIGHT = 1000
-x = 120
-y = 700
 
-x2 = 990
-points = 0
 
 py.init()
-
-
 
 ekran = py.display.set_mode([SCREEN_WIDHT, SCREEN_HEIGHT])
 zegar = py.time.Clock()
@@ -26,39 +18,22 @@ obraz_tla = py.image.load("background.png")
 PORUSZ_AUTEM = py.USEREVENT+1
 py.time.set_timer(PORUSZ_AUTEM, 10)
 
-
-
-
-
-
 gra_dziala = True
-
-#pozycja = x2, 0
-y2 = -200
-y3 = -200
-
-
-x3 = random.randrange(120, 638 , 216)
-x2 = random.randrange(120, 638 , 216)
 
 class Auto1():
     def __init__(self):
-        global x2
-        self.x2 = x2
-        global y2
-        self.y2 = y2
-        self.hitbox = py.Rect(self.x2, self.y2, 180, 280)
+        self.x = 990
+        self.y = -200
+        self.hitbox = py.Rect(self.x, self.y, 180, 280)
         self.image = py.image.load('car1.png')
     
     def update_hitbox(self):
-        self.hitbox.topleft = (self.x2 , self.y2)
+        self.hitbox.topleft = (self.x , self.y)
     
 class Taksowka():
     def __init__(self):
-        global x
-        self.x = x
-        global y
-        self.y = y
+        self.x = 120
+        self.y = 700
         self.hitbox = py.Rect(self.x, self.y, 155, 281)
         self.image = py.image.load('TAXI.png')
     
@@ -67,36 +42,46 @@ class Taksowka():
 
 class Auto2():
     def __init__(self):
-        global x3
-        global y3
-        self.x3 = x3
-        self.y3 = y3
-        self.hitbox = py.Rect(self.x3, self.y3, 180, 280)
+        self.x = 990
+        self.y = -200
+        self.hitbox = py.Rect(self.x, self.y , 180, 280)
         self.image = py.image.load('car2.png')
     
     def update_hitbox(self):
-        self.hitbox.topleft = (self.x3 , self.y3)
+        self.hitbox.topleft = (self.x , self.y)
+
+
 
 taksowka = Taksowka()
 auto1 = Auto1()
 auto2 = Auto2()
 
-while x2 == x3:
-    x3 = random.randrange(120, 638 , 216)
-    x2 = random.randrange(120, 638 , 216)
+auto1.y = -200
+auto2.y = -200
 
-pozycja = x2, y2
-pozycja1 = x3, y3
+auto2.x = random.randrange(120, 638 , 216)
+auto1.x = random.randrange(120, 638 , 216)
+
+taksowka.x = 120
+taksowka.y = 700
+
+auto1.x = 990
+points = 0
+
+while auto1.x == auto2.x:
+    auto1.x = random.randrange(120, 638 , 216)
+    auto2.x = random.randrange(120, 638 , 216)
+
+#pozycja = auto1.x, auto1.y
+#pozycja1 = x3, y3
 
 a = random.randint(8, 13)
 b = random.randint(8, 13)
 
 while gra_dziala:
 
-    pozycja = x2, y2
-    pozycja1 = x3, y3
-    y2 += a
-    y3 += b
+    auto1.y += a
+    auto2.y += b
 
     auto1.update_hitbox()
     taksowka.update_hitbox()
@@ -107,30 +92,30 @@ while gra_dziala:
         if zdarzenie.type == py.QUIT:
                 gra_dziala = False
 
-        if y2 >= 1000:
-             x2 = random.randrange(120, 638 , 216)
-             y2 = -600
+        if auto1.y >= 1000:
+             auto1.x = random.randrange(120, 638 , 216)
+             auto1.y = -600
              points += 1
 
              a = random.randint(8, 13)
              #b = random.randint(8, 13)
              
-             while x3 == x2:
-                x2 = random.randrange(120, 638 , 216)
+             while auto2.x == auto1.x:
+                auto1.x = random.randrange(120, 638 , 216)
             
              auto1.update_hitbox()
              taksowka.update_hitbox()
              auto2.update_hitbox()
         
-        if y3 >= 1000:
-            x3 = random.randrange(120, 638 , 216)
-            y3 = -600
+        if auto2.y >= 1000:
+            auto2.x = random.randrange(120, 638 , 216)
+            auto2.y = -600
 
             #a = random.randint(8, 13)
             b = random.randint(8, 13)
 
-            while x3 == x2:
-                 x3 = random.randrange(120, 638 , 216)
+            while auto2.x == auto1.x:
+                 auto2.x = random.randrange(120, 638 , 216)
             
             auto1.update_hitbox()
             taksowka.update_hitbox()
@@ -144,19 +129,19 @@ while gra_dziala:
                 gra_dziala = False
                 
 
-            if x > 120:
+            if taksowka.x > 120:
                 if zdarzenie.key == py.K_a:
-                     x -= 108
-                     x -= 108
+                     taksowka.x -= 108
+                     taksowka.x -= 108
 
                 auto1.update_hitbox()
                 taksowka.update_hitbox()
                 auto2.update_hitbox()
                 
-            if x < 548:
+            if taksowka.x < 548:
                 if zdarzenie.key == py.K_d:
-                    x += 108
-                    x += 108
+                    taksowka.x += 108
+                    taksowka.x += 108
                 
                 auto1.update_hitbox()
                 taksowka.update_hitbox()
